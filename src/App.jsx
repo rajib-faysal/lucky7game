cee42e650cbcc03ac250403bad2e5975cccbf190
K. M. Rajib Faysal <rajib.faysal.201@gmail.com>
2024-08-24 19:54:17 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+        This is a template for minimal quick start of react project using Vite
+      </h1>
+    </>
+  );
+}
+
+export default App;
